17d623f01a34edcc44ffa65223a46e3fe51c022a
lucia <lucia@LUCIALEYVA123>
2024-10-24 08:58:21 -0700

    Agregar archivos de proyecto.

---
+++ Perros_API/ImagenesDePerros.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ImagenesDePerros.aspx.cs" Inherits="Perros_API.ImagenesDePerros" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Imagenes Aleatorias de Perros</title>
+</head>
+<body>
+    <form id="form1" runat="server" style="text-align: center; margin-top: 50px;">
+        <h1>--Imagenes aleatorias de Perros--</h1>
+        <asp:Image ID="imgPerro" runat="server" Width="300px" Height="300px" />
+        <br /><br />
+        <asp:Button ID="btnObtenerImagen" runat="server" Text="Obtener Imagen" OnClick="btnObtenerImagen_Click" />
+    </form>
+</body>
+</html>
---
+++ Perros_API/ImagenesDePerros.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace Perros_API
+{
+    public partial class ImagenesDePerros : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ObtenerImagenPerro();
+            }
+        }
+        protected void btnObtenerImagen_Click(object sender, EventArgs e)
+        {
+            ObtenerImagenPerro();
+        }
+
+        private void ObtenerImagenPerro()
+        {
+            string urlApi = "https://dog.ceo/api/breeds/image/random";
+
+            HttpWebRequest solicitud = (HttpWebRequest)WebRequest.Create(urlApi);
+            solicitud.Method = "GET";
+
+            using (HttpWebResponse respuesta = (HttpWebResponse)solicitud.GetResponse())
+            {
+                using (StreamReader lector = new StreamReader(respuesta.GetResponseStream()))
+                {
+                    string cuerpoRespuesta = lector.ReadToEnd();
+
+                    JObject json = JObject.Parse(cuerpoRespuesta);
+                    string urlImagen = json["message"].ToString();
+
+                    imgPerro.ImageUrl = urlImagen;
+                }
+            }
+        }
+    }
+}
